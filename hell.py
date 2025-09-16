7235a4fc3f9208984d7a9b9ebd57a15a95a377d2
Marcelloh <koilacodewars29@gmail.com>
2025-09-16 11:28:58 +0100

    Added some of python factorial itertools

--- hell.py
+++ hell.py
@@ -2,10 +2,16 @@ def factorial(n):
     if n == 0:
         return 1
     else:
-        print n * factorial(n - 1)
+        print(n * factorial(n - 1))
         
 number = 5
 result = factorial(number)
 print(f"The factorial of {number} is {result}.")
 print("This is the second line that should be printed in blue color")
 
+from itertools import imap
+def factorial(x):
+    return reduce(long.__mul__, imap(long, xrange(1, x + 1)))
+
+
+print factorial(1000)
