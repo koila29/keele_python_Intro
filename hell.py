e07938d3511a7785647832811e6cf1eb3cadd7b8
Marcelloh <koilacodewars29@gmail.com>
2025-09-16 11:05:10 +0100

    I have added the name of the main repository

--- hell.py
+++ hell.py
@@ -8,3 +8,5 @@ number = 5
 result = factorial(number)
 print(f"The factorial of {number} is {result}.")
 print("This is the second line that should be printed in blue color")
+
+#This is the name of the main repository
